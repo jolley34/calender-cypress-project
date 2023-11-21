8236c9390e8658457608bb58eb46603d8b3639d7
jolley34 <erlandssonjoel@gmail.com>
2023-11-21 19:00:37 +0100

    skapade ett enkelt grid system till kalender dagar

--- public/main.js
+++ public/main.js
@@ -5,4 +5,17 @@ function main() {
   // Init today view
   // Init todos list
   // Init calendar
+  const calendar = document.querySelector(".app-calendar")
+
+  // Hämta dagarna
+  for (let day = 1; day <= 31; day++) {
+    // Skapar grid-card elementet
+    const dayCard = document.createElement("div");
+    // Skapar klassnamnet på grid-card elementet
+    dayCard.classList.add("day-card");
+    // appendar in dag funktioner in i dayCard Diven
+    dayCard.textContent = `${day}`;
+    calendar.append(dayCard);
+
+  }
 }
