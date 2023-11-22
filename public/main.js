f41c52e434c525fda1facddf23bf1a970ff66f28
jolley34 <erlandssonjoel@gmail.com>
2023-11-22 13:35:33 +0100

    okerokeokroer

--- public/main.js
+++ public/main.js
@@ -1,11 +1,16 @@
-window.addEventListener('DOMContentLoaded', main);
+window.addEventListener("DOMContentLoaded", main);
+
+const MONTH_NAMES = Array.from({ length: 12 }, (_, i) => {
+  return new Date(0, i).toLocaleString("en-US", { month: "long" });
+});
+console.log(MONTH_NAMES);
 
 function main() {
-  console.log('Hi students!');
+  console.log("Hi students!");
   // Init today view
   // Init todos list
   // Init calendar
-  const calendar = document.querySelector(".app-calendar")
+  const calendar = document.querySelector(".app-calendar");
 
   // Hämta dagarna
   for (let day = 1; day <= 31; day++) {
@@ -16,6 +21,10 @@ function main() {
     // appendar in dag funktioner in i dayCard Diven
     dayCard.textContent = `${day}`;
     calendar.append(dayCard);
-
   }
+
+  const month = document.querySelector(".month");
+  const monthElement = document.createElement("h1");
+  monthElement.classList.add("month");
+  
 }
