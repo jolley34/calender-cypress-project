4e1f1a79610ca2a0c197f7a0168aa56ba8d7f4eb
malin <malleballe90@gmail.com>
2023-11-23 13:49:48 +0100

    a modal to edit todo items

--- public/todo.js
+++ public/todo.js
@@ -5,12 +5,22 @@ function main (){
 }
 
 function addTodo(){
+    //add todo form
     const todolist = document.getElementById("todo-list");
     const newTodoInput = document.getElementById("new-todo");
     const todoDateInput = document.getElementById("todo-date")
     const todoTimeInput = document.getElementById("todo-time")
     const addTodoBtn = document.getElementById("add-todo-btn")
 
+    //Edit modat to edit todos
+    const editModal = document.getElementById("edit-modal");
+    const editTodoText = document.getElementById("edit-todo-text");
+    const editTodoDate = document.getElementById("edit-todo-date");
+    const editTodoTime = document.getElementById("edit-todo-time");
+    const saveEditBtn = document.getElementById("save-edit-btn");
+    const closeEditBtn = document.getElementById("close-edit-btn");
+
+    // adds the todos to list when add-button is clicked
     addTodoBtn.addEventListener('click', function(){
         const todoText = newTodoInput.value.trim();
         const todoDate = todoDateInput.value;
@@ -22,10 +32,43 @@ function addTodo(){
             todoItem.textContent = `${todoText} - Time: ${todoTime} - ${todoDate}`
             todolist.appendChild(todoItem);
 
+            //clears input after click on add-button
             newTodoInput.value = "";
             todoTimeInput.value = "";
             todoDateInput.value = "";
+
+            todoItem.addEventListener('click', function(){
+
+                editTodoText.value = todoText;
+                editTodoTime.value = todoTime;
+                editTodoDate.value = todoDate;
+
+                editModal.style.display = "block";
+
+            });
         }
+    });
+
+    saveEditBtn.addEventListener('click', function (){
+
+        const editedText = editTodoText.value.trim();
+        const editedTime = editTodoTime.value;
+        const editedDate = editTodoDate.value;
+
+        const editedItem = document.createElement("li");
+        editedItem.className = "todo-item";
+        editedDate.innerHTML = `${editedText} - Time: ${editedTime} - ${editedDate}`;
+
+        const originalItem = editModal.previousSibling;
+        originalItem.parentNode.replaceChild(editedItem, originalItem);
+
+        editModal.style.display = "none";
+
+    });
+
+    closeEditBtn.addEventListener('click', function(){
+
+        editModal.style.display = "none";
     });
 
 }
