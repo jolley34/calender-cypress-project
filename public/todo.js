2a65d4068e362e520775bfe46699060b4c144d82
malin <malleballe90@gmail.com>
2023-11-27 14:20:16 +0100

    puuulll

--- public/todo.js
+++ public/todo.js
@@ -16,13 +16,6 @@ function addTodo(){
     const todoEndTimeInput = document.getElementById('todo-end-time')
     const addTodoBtn = document.getElementById("add-todo-btn")
 
-    //Edit modal to edit todos
-   // const editModal = document.getElementById("edit-modal");
-   // const editTodoText = document.getElementById("edit-todo-text");
-   // const editTodoDate = document.getElementById("edit-todo-date");
-    //const editTodoTime = document.getElementById("edit-todo-time");
-    //const saveEditBtn = document.getElementById("save-edit-btn");
-    //const closeEditBtn = document.getElementById("close-edit-btn");
 
     // adds the todos to list when add-button is clicked
     addTodoBtn.addEventListener('click', function(){
@@ -76,27 +69,4 @@ todoItem.addEventListener('click', function(){
 
 
 
-    /*
-    editModal.style.display = "block";
-    saveEditBtn.addEventListener('click', function (){
-
-        const editedText = editTodoText.value.trim();
-        const editedTime = editTodoTime.value;
-        const editedDate = editTodoDate.value;
-
-        const editedItem = document.createElement("li");
-        editedItem.className = "todo-item";
-        editedDate.innerHTML = `${editedText} - Time: ${editedTime} - ${editedDate}`;
-
-        const originalItem = editModal.previousSibling;
-        originalItem.parentNode.replaceChild(editedItem, originalItem);
-
-        editModal.style.display = "none";
-
-    });
-
-    closeEditBtn.addEventListener('click', function(){
-
-        editModal.style.display = "none";
-    });
-    */
+ 
