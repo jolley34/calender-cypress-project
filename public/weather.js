b0d896bb462edfd06abcf07220f48ba2aac6c3b6
jolley34 <erlandssonjoel@gmail.com>
2023-11-29 13:12:14 +0100

    git init

--- public/weather.js
+++ public/weather.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
   canvas.height = window.innerHeight;
 
   let particles = [];
-  let weatherType = 'none'; // Default to 'none'
+  let weatherType = "none";
 
   function createParticlesForSnow() {
     const particleCount = 300;
@@ -38,9 +38,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function createParticles(weather) {
     particles = [];
-    if (weather === 'snow') {
+    if (weather === "snow") {
       createParticlesForSnow();
-    } else if (weather === 'rain') {
+    } else if (weather === "rain") {
       createParticlesForRain();
     }
   }
@@ -48,14 +48,18 @@ window.addEventListener("DOMContentLoaded", () => {
   function updateParticleMovement() {
     particles.forEach((particle) => {
       particle.y += particle.speed;
-      if (weatherType === 'snow') {
+      if (weatherType === "snow") {
         if (particle.y > canvas.height) {
           particle.y = 0;
           particle.x = Math.random() * canvas.width;
         }
       } else {
         particle.x += Math.sin(particle.direction);
-        if (particle.y > canvas.height || particle.x > canvas.width || particle.x < 0) {
+        if (
+          particle.y > canvas.height ||
+          particle.x > canvas.width ||
+          particle.x < 0
+        ) {
           particle.y = 0;
           particle.x = Math.random() * canvas.width;
         }
@@ -65,7 +69,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function drawParticles() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = weatherType === 'snow' ? "white" : "#89CFF0";
+    ctx.fillStyle = weatherType === "snow" ? "white" : "#89CFF0";
 
     particles.forEach((particle) => {
       ctx.beginPath();
@@ -91,37 +95,40 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   async function fetchWeather() {
-    const apiKey = '04cefb85cb8ecd887ad8e9600dddc436';
-    const location = 'gothenburg';
+ /*    const apiKey = "04cefb85cb8ecd887ad8e9600dddc436"; */
+    const location = "gothenburg";
 
     try {
-      const response = await fetch(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${location}`);
+      const response = await fetch(
+        `http://api.weatherstack.com/current?access_key=${apiKey}&query=${location}`
+      );
       const data = await response.json();
 
       if (data.error) {
         throw new Error(data.error.info);
       }
 
-      const weatherDescription = data.current.weather_descriptions[0].toLowerCase();
+      const weatherDescription =
+        data.current.weather_descriptions[0].toLowerCase();
 
-      if (weatherDescription.includes('snow')) {
-        weatherType = 'snow';
-      } else if (weatherDescription.includes('rain')) {
-        weatherType = 'rain';
+      if (weatherDescription.includes("snow")) {
+        weatherType = "snow";
+      } else if (weatherDescription.includes("rain")) {
+        weatherType = "rain";
       } else {
-        weatherType = 'none';
+        weatherType = "none";
       }
 
       createParticles(weatherType);
 
       const temperatureCelsius = data.current.temperature;
       const weatherText = `${weatherDescription} ${temperatureCelsius}°C`;
-      const locationWeather = document.querySelector('.location-weather');
+      const locationWeather = document.querySelector(".location-weather");
       locationWeather.textContent = weatherText;
     } catch (error) {
-      console.error('Error fetching weather data:', error.message);
-      const locationWeather = document.querySelector('.location-weather');
-      locationWeather.textContent = 'Failed to fetch weather';
+      console.error("Error fetching weather data:", error.message);
+      const locationWeather = document.querySelector(".location-weather");
+      locationWeather.textContent = "Failed to fetch weather";
     }
   }
 
